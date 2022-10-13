d41f2908ca394c8f060d95e89cd2c3813fe556c4
Daniel Don <dondaniel028@gmail.com>
2022-10-13 04:59:23 +0100

    Project boilerplate

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="text-5xl">Sevenz Healthcare Frontend Lead test.</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "IndexPage",
+});
+</script>
